d41e3e00ccfb600a0f8a0872e2999a9046eba274
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2023-01-10 16:06:06 +0800

    Format Files

--- proxyScript.js
+++ proxyScript.js
@@ -12,56 +12,64 @@ let proxies;
 /**
  * @param domains string[]
  */
-function setProxiedDomains(domains) {
-	proxiedDomains = domains;
+function setProxiedDomains(domains)
+{
+    proxiedDomains = domains;
 }
 
 //noinspection JSIgnoredPromiseFromCall
 browser.runtime.sendMessage(['from_proxy_script', 'init_proxy_script']);
 
-browser.runtime.onMessage.addListener(async (message) => {
-	const task = message[0];
-	const data = message[1];
-	switch (task) {
-		case 'set_proxied_domains':
-			setProxiedDomains(data);
-			break;
-		case 'set_state':
-			proxyIsEnabled = data;
-			break;
-		case 'set_proxies':
-			proxies = data;
-			break;
-		default:
-			// Logging
-			await browser.runtime.sendMessage(['from_proxy_script', `ERROR: invalid task '${task}'`]);
-	}
-	// Logging
-	await browser.runtime.sendMessage(['from_proxy_script', `ACK: ${task} ${data}`]);
+browser.runtime.onMessage.addListener(async (message) =>
+{
+    const task = message[0];
+    const data = message[1];
+    switch (task)
+    {
+        case 'set_proxied_domains':
+            setProxiedDomains(data);
+            break;
+        case 'set_state':
+            proxyIsEnabled = data;
+            break;
+        case 'set_proxies':
+            proxies = data;
+            break;
+        default:
+            // Logging
+            await browser.runtime.sendMessage(['from_proxy_script', `ERROR: invalid task '${task}'`]);
+    }
+    // Logging
+    await browser.runtime.sendMessage(['from_proxy_script', `ACK: ${task} ${data}`]);
 });
 
-function getProxy() {
-	return proxies.join('; ');
+function getProxy()
+{
+    return proxies.join('; ');
 }
 
-function findProxyForDomain(domain) {
-	const direct = 'DIRECT';
-	if (!proxyIsEnabled) {
-		return direct;
-	}
-	const isProxiedDomain = proxiedDomains.includes(domain);
-	if (!isProxiedDomain) {
-		return direct;
-	}
-	const proxy = getProxy();
-	//noinspection JSIgnoredPromiseFromCall
-	browser.runtime.sendMessage(['from_proxy_script', [proxy, domain]]); // Logging
-	return proxy;
+function findProxyForDomain(domain)
+{
+    const direct = 'DIRECT';
+    if (!proxyIsEnabled)
+    {
+        return direct;
+    }
+    const isProxiedDomain = proxiedDomains.includes(domain);
+    if (!isProxiedDomain)
+    {
+        return direct;
+    }
+    const proxy = getProxy();
+    //noinspection JSIgnoredPromiseFromCall
+    browser.runtime.sendMessage(['from_proxy_script', [proxy, domain]]); // Logging
+    return proxy;
 }
 
 //noinspection JSUnusedGlobalSymbols
-function FindProxyForURL(url, host) {
-	const proxy = findProxyForDomain(host);
-	//noinspection JSConstructorReturnsPrimitive
-	return proxy;
+function FindProxyForURL(url, host)
+{
+    const proxy = findProxyForDomain(host);
+    //noinspection JSConstructorReturnsPrimitive
+    return proxy;
 }
